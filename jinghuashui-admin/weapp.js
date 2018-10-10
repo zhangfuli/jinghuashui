9222dd84642252414570e4feaf3add80f1db59bd
zhangfuli <1171067930@qq.com>
2018-10-10 17:40:30 +0800

    净化水-admin

---
+++ jinghuashui-admin/weapp.js
@@ -0,0 +1,10 @@
+const weapp = {
+  "usingComponents": {
+      "i-button": "../../dist/button/index",
+      "i-tab-bar": "../../dist/tab-bar/index",
+      "i-tab-bar-item": "../../dist/tab-bar-item/index"
+  }
+}
+module.exports = { //数据暴露出去
+  weapp: weapp
+}
